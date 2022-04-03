96c84f504bb3362f404424daaec3a2db2c4a54dc
Khurrambek <xurrambeks@gmail.com>
2022-04-04 03:40:15 +0500

    alert message

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017", { useNewUrlParser: true })
+  .connect(
+    "mongodb+srv://xurrambek:xurram200`@student-database.swezn.mongodb.net/student-database?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+    }
+  )
   .then(() => console.log("connected to database"))
   .catch((err) => console.log(err));
 
